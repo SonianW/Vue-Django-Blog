ecf1a5e097c9930553e034c6fd501243da39dfa2
Sonian <wangjiaqiang.sonian@bytedance.com>
2022-08-30 20:32:13 +0800

    dev article list serializers, views

--- blog/article/serializers.py
+++ blog/article/serializers.py
@@ -1,10 +1,13 @@
-from pickletools import read_long1
-from turtle import update
+from dataclasses import field
 from rest_framework import serializers
+from article.models import Article
 
-class ArticleListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(allow_blank=True, max_length=100)
-    body = serializers.CharField(allow_blank=True)
-    created = serializers.DateTimeField()
-    updated = serializers.DateTimeField()
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'author',
+            'title',
+            'created',
+        ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from article.models import Article
 from article.serializers import ArticleListSerializer
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleListSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
