bb847ada4d600e4a75dac435278b8abb4f073b81
Sonian <wangjiaqiang.sonian@bytedance.com>
2022-08-30 20:18:18 +0800

    dev article app

---
+++ blog/article/serializers.py
@@ -0,0 +1,10 @@
+from pickletools import read_long1
+from turtle import update
+from rest_framework import serializers
+
+class ArticleListSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(allow_blank=True, max_length=100)
+    body = serializers.CharField(allow_blank=True)
+    created = serializers.DateTimeField()
+    updated = serializers.DateTimeField()
